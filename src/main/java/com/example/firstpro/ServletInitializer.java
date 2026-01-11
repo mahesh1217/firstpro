b881b21353066042a534c099972bb16f209bed1a
mahesh1217 <maheshkoppineedi7@gmail.com>
2026-01-12 00:12:30 +0530

    Initial commit

---
+++ src/main/java/com/example/firstpro/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.firstpro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(FirstproApplication.class);
+	}
+
+}
---
+++ src/test/java/com/example/firstpro/FirstproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
